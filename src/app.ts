f7384bec9e607f9dd5a077f313da1d624cec9fb0
Jasur <jasurbekshomaqsudov2004@gmail.com>
2023-12-26 19:34:56 +0300

    add type=commonjs to package.json

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,13 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // app.use(express.json())
-app.use(cors())
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // app.use((req: Request, res: Response, next: NextFunction) => {
 //   res.header('Access-Control-Allow-Origin', '*')
